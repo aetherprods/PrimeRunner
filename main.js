09e4f9c5e084c05ec6ed390b0d068b8752fb2640
Jorge Barreto <jbarreto@localhost.localdomain>
2019-06-29 12:36:55 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,48 @@
+let primeContestant = Math.floor(Math.random() * 5000);
+if (primeContestant % 2 == 0) {
+    primeContestant +=1;
+}
+
+
+$( document ).ready(function() {
+    
+    document.getElementById("primer").innerHTML = primeContestant;
+
+});
+
+function checkforPrimeness (num) {
+    let primeness = true;
+    for(let i=2; i<=Math.sqrt(num); i++) {
+        if (num%i == 0) {
+            primeness = false;
+            return primeness;
+        } else {
+            primeness = true;
+        }
+    }
+    return primeness;
+}
+
+
+function isPrime () {
+    if (checkforPrimeness(primeContestant) == true) {
+        alert("right!");
+    } else {
+        alert("wrong!");
+    }
+    newPrime();
+}
+
+function isNotPrime () {
+    if (checkforPrimeness(primeContestant) == false) {
+        alert("right!");
+    } else {
+        alert("wrong!");
+    }
+    newPrime();
+}
+
+function newPrime () {
+    primeContestant = Math.floor(Math.random() * 5000);
+    document.getElementById("primer").innerHTML = primeContestant;
+}
