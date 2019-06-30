08b8a3a5f95963b79b8487fccd42adc76ddcd6e8
Jorge Barreto <jbarreto@localhost.localdomain>
2019-06-29 20:06:48 -0400

    added score counter

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
 let primeContestant = 1;
+let score = 0;
+
+
+
+
 
 $( document ).ready(function() {
-    newPrime();
-    document.getElementById("primer").innerHTML = primeContestant;
 
+    newPrime();
+   // timer();
 });
+
+function timer() {
+    let now = new Date();
+    now = now.getTime();
+    let thirtySeconds = now+30000;
+
+    for (i=now; i<thirtySeconds; i++) {
+        document.getElementById("timer").innerHTML = i;
+    }
+
+}
+
+function counter(outcome) {
+    if (outcome == "correct") {
+        score++;
+    } else if (outcome == "incorrect") {
+        score--;
+    }
+    document.getElementById("counter").innerHTML = score;
+}
+
 
 function checkforPrimeness (num) {
     let primeness = true;
-    for(let i=2; i<=Math.sqrt(num); i++) {
+    for(let i=7; i<=Math.sqrt(num); i++) {
         if (num%i == 0) {
             primeness = false;
             return primeness;
@@ -22,29 +48,39 @@ function checkforPrimeness (num) {
 
 function isPrime () {
     if (checkforPrimeness(primeContestant) == true) {
-        alert("right!");
+        document.getElementById("last-result").innerHTML = "Correct!";
+        document.getElementById("last-result").style.backgroundColor = "green";
+        counter("correct");
     } else {
-        alert("wrong!");
+        document.getElementById("last-result").innerHTML = "Incorrect!";
+        document.getElementById("last-result").style.backgroundColor = "red";
+        counter("incorrect");
     }
     newPrime();
 }
 
 function isNotPrime () {
     if (checkforPrimeness(primeContestant) == false) {
-        alert("right!");
+        document.getElementById("last-result").innerHTML = "Correct!";
+        document.getElementById("last-result").style.backgroundColor = "green";
+        counter("correct");
     } else {
-        alert("wrong!");
+        document.getElementById("last-result").innerHTML = "Incorrect!";
+        document.getElementById("last-result").style.backgroundColor = "red";
+        counter("incorrect");
     }
     newPrime();
 }
 
 function newPrime () {
-    primeContestant = Math.floor(Math.random() * 5000);
+    primeContestant = Math.floor(Math.random() * 25000);
     if (primeContestant % 2 == 0) {
         newPrime();
     } else if ((primeContestant % 3 == 0)) {
         newPrime();
     } else if ((primeContestant % 5 == 0)) {
+        newPrime();
+    } else if ((primeContestant % 7 == 0)) {
         newPrime();
     } 
     document.getElementById("primer").innerHTML = primeContestant;
