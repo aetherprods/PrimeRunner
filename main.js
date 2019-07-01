21c49bb2022f1fa1fd9ad644eb32778c55ccf9d0
Jorge Barreto <jbarreto@localhost.localdomain>
2019-07-01 17:23:52 -0400

    fix broken

--- main.js
+++ main.js
@@ -1,112 +1,96 @@
-//import * as $ from 'jquery'
-
-
-
-
-
-
 $( document ).ready(function() {
 
-    game = new game(30);
+    gameStart();
+
+    
 
 });
 
-/* currently being kept for reference
-function startTimer() {
-    var timer = 300, seconds;
 
-    setInterval(function () {
-        seconds = parseInt(timer, 10);
-        document.querySelector('#timer').textContent = seconds;
 
-        //document.querySelector('#timer').textContent = minutes.toString(10).slice(0,2) + ":" + minutes.toString(10).slice(2);
-
-        if (--timer < 0) {
-            timer = 30;
-        }
-    }, 100);
-}
-*/
-
-function timer(duration) {
-    //this.timer = timer;
+function countingTimer(duration) {
+    
     this.duration = duration;
     this.running = false;
     this.timeInterval;
-    //document.querySelector('#timer').textContent = timer.start();
-    document.querySelector('#timer').textContent = duration.toString(10).slice(0,2) + ":" + duration.toString(10).slice(2) + "0";
-
+    
 }
 
-timer.prototype.start = (duration) => {
-    this.timer = timer;
-    var time = duration, seconds;
+countingTimer.prototype.start = function(duration) {
+    let that = this,
+        time = duration,
+        seconds;
 
-    if (timer.running == false) {
-        timer.running = true;
+    if (that.running == false) {
+        that.running = true;
 
     timeInterval = setInterval(function () {
             seconds = parseInt(time, 10);
-            document.querySelector('#timer').textContent = timer.parse(seconds);
+            document.querySelector('#timer').textContent = that.parse(seconds);
 
             if (--time < 0) {
-                timer.running = false;
-                timer.stopTimer();
+                that.running = false;
+                that.stopTimer();
             }
         }, 100);
     }
 }
 
-timer.prototype.stopTimer = () => {
+countingTimer.prototype.stopTimer = function() {
     clearInterval(timeInterval);
-    game.stopGame();
+    primeGame.stopGame();
 }
 
-timer.prototype.parse = (seconds) => {
+countingTimer.prototype.parse = function(time) {
+    let that = this;
  //   this.seconds = seconds;
-    seconds = seconds.toString(10).slice(0,2) + ":" + seconds.toString(10).slice(2) + "0";
-    return seconds;
-}
-
-
-
-function game(duration) {
-    this.primeContestant;
-    this.score;
-    this.running = false;
-    this.duration = duration;
-    //put in key bindinds
-    timer = new timer(duration);
-    
-    document.querySelector('#startButton').addEventListener('click', () => {
-        timer.start(duration)
-        game.startGame();
-    });
-
-}
-
-game.prototype.startGame = () => {
-    document.querySelector('#isPrime').addEventListener('click', () => game.isPrime());
-    document.querySelector('#isNotPrime').addEventListener('click', () => game.isNotPrime());
-    game.score = 0;
-}
-
-game.prototype.stopGame = () => {
-    alert("game over");
-    document.querySelector('#isPrime').removeEventListener('click', () => game.isPrime());
-    document.querySelector('#isNotPrime').removeEventListener('click', () => game.isNotPrime());
-}
-
-game.prototype.counter = (outcome) => {
-    if (outcome == "correct") {
-        game.score++;
-    } else if (outcome == "incorrect") {
-        game.score--;
+    if (time > 600) {
+        let minutes = time/600 | 0;
+        let seconds = time%600 | 0;
+        time = "0" + minutes + ":" + "0" + seconds.toString(10).slice(0,2) + seconds.toString(10).slice(2)
+    } else if (time <= 600) {
+        time = time.toString(10).slice(0,2) + ":" + time.toString(10).slice(2) + "0";
+    } else if (time < 100) {
+        time = "0" + time.toString(10).slice(0,1) + ":" + time.toString(10).slice(1) + "0";
     }
-    document.getElementById("counter").innerHTML = game.score;
+
+    return time;
 }
 
-game.prototype.checkforPrimeness = (num) => {
+
+
+function primeGame() {
+    this.primeContestant = 0;
+    this.score = 0;
+    this.running = false;
+}
+
+primeGame.prototype.startGame = function() {
+    let that = this;
+    document.querySelector('#isPrime').addEventListener('click', () => that.isPrime());
+    document.querySelector('#isNotPrime').addEventListener('click', () => that.isNotPrime());
+    that.score = 0;
+}
+
+primeGame.prototype.stopGame = function() {
+    let that = this;
+    alert("game over");
+    document.querySelector('#isPrime').removeEventListener('click', () => that.isPrime());
+    document.querySelector('#isNotPrime').removeEventListener('click', () => that.isNotPrime());
+    gameStart();
+}
+
+primeGame.prototype.counter = function(outcome) {
+    let that = this;
+    if (outcome == "correct") {
+        that.score++;
+    } else if (outcome == "incorrect") {
+        that.score--;
+    }
+    document.getElementById("counter").innerHTML = that.score;
+}
+
+primeGame.prototype.checkforPrimeness = function(num) {
     let primeness = true;
     for(let i=7; i<=Math.sqrt(num); i++) {
         if (num%i == 0) {
@@ -119,42 +103,60 @@ game.prototype.checkforPrimeness = (num) => {
     return primeness;
 }
 
-game.prototype.isPrime = () => {
-    if (game.checkforPrimeness(game.primeContestant) == true) {
+primeGame.prototype.isPrime = function() {
+    let that = this;
+    if (that.checkforPrimeness(that.primeContestant) == true) {
         document.getElementById("last-result").innerHTML = "Correct!";
         document.getElementById("last-result").style.backgroundColor = "green";
-        game.counter("correct");
+        that.counter("correct");
     } else {
         document.getElementById("last-result").innerHTML = "Incorrect!";
         document.getElementById("last-result").style.backgroundColor = "red";
-        game.counter("incorrect");
+        that.counter("incorrect");
     }
-    game.newPrime();
+    that.newPrime();
 }
 
-game.prototype.isNotPrime = () => {
-    if (game.checkforPrimeness(game.primeContestant) == false) {
+primeGame.prototype.isNotPrime = function() {
+    let that = this;
+    if (that.checkforPrimeness(that.primeContestant) == false) {
         document.getElementById("last-result").innerHTML = "Correct!";
         document.getElementById("last-result").style.backgroundColor = "green";
-        game.counter("correct");
+        that.counter("correct");
     } else {
         document.getElementById("last-result").innerHTML = "Incorrect!";
         document.getElementById("last-result").style.backgroundColor = "red";
-        game.counter("incorrect");
+        that.counter("incorrect");
     }
-    game.newPrime();
+    that.newPrime();
 }
 
-game.prototype.newPrime = () => {
-    game.primeContestant = Math.floor(Math.random() * 25000);
-    if (game.primeContestant % 2 == 0) {
-        game.newPrime();
-    } else if ((game.primeContestant % 3 == 0)) {
-        game.newPrime();
-    } else if ((game.primeContestant % 5 == 0)) {
-        game.newPrime();
-    } else if ((game.primeContestant % 7 == 0)) {
-        game.newPrime();
+primeGame.prototype.newPrime = function() {
+    let that = this;
+    that.primeContestant = Math.floor(Math.random() * 25000);
+    if (that.primeContestant % 2 == 0) {
+        that.newPrime();
+    } else if ((that.primeContestant % 3 == 0)) {
+        that.newPrime();
+    } else if ((that.primeContestant % 5 == 0)) {
+        that.newPrime();
+    } else if ((that.primeContestant % 7 == 0)) {
+        that.newPrime();
     } 
-    document.getElementById("primer").innerHTML = game.primeContestant;
+    document.getElementById("primer").innerHTML = that.primeContestant;
+}
+
+
+function gameStart() {
+
+    
+    game = new primeGame();
+    timer = new countingTimer(30);
+    
+    document.querySelector('#timer').textContent = timer.parse(30);
+    
+    document.querySelector('#startButton').addEventListener('click', () => {
+        timer.start(30)
+        game.startGame();
+    });
 }
