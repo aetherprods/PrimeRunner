b5d03b81a408dfb34086178c605ac45f4d62c530
Jorge Barreto <jbarreto@localhost.localdomain>
2019-07-02 04:52:03 -0400

    express test

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-
 const express = require('express'),
       port = process.env.PORT || 3000;
       app = express(),
 
 app.get('/poop', (req, res) => res.send("Hello World!"));
-app.get('/', (req, res) => {
-return res.send("Hello World!");
-});
+app.get('/', (req, res) => {res.sendFile(__dirname + "/main.html")});
 app.listen(port, () => console.log("i am zord"));
