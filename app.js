83851cff514707ec5b57039da3d0a64da89f38c9
Jorge Barreto <jbarreto@localhost.localdomain>
2019-07-02 10:45:46 -0400

    connect mongodb db instance

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ let data=[
     {"b": 2},
     {"c": 3},
 ]
-let uri = "mongodb://heroku_76n6c1p1:G`7vyajtrW'G_(23@ds245927.mlab.com:45927/heroku_76n6c1p1" 
+let uri = "mongodb://heroku_76n6c1p1:Gf7vyajtrWdfgG6854sdrfg23@ds245927.mlab.com:45927/heroku_76n6c1p1" 
 
 mongodb.MongoClient.connect(uri, (err, client) => {
     if(err) throw err;
