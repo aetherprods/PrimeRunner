b74f091e185aa31a1609709680e85c0b49fa8e4f
Jorge Barreto <jbarreto@localhost.localdomain>
2019-07-02 10:43:53 -0400

    connect mongodb db instance

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 const express = require('express')
       path = require('path')
+      mongodb = require('mongodb')
       mongoose = require('mongoose')
       port = process.env.PORT || 3000
       app = express();
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://heroku_76n6c1p1:u0t3c6j9bqa5c6e6ikbsprtooj@ds245927.mlab.com:45927/heroku_76n6c1p1");
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://heroku_76n6c1p1:G`7vyajtrW'G_(23@ds245927.mlab.com:45927/heroku_76n6c1p1");
 
 app.get('/', (req, res) => {res.sendFile(__dirname + "/main.html")});
 //app.use(express.static(__dirname + "/public"));
 app.use(express.static(path.join(__dirname, "/")));
 app.listen(port, () => console.log("i am zord"));
 
+
+let data=[
+    {"a": 1},
+    {"b": 2},
+    {"c": 3},
+]
+let uri = "mongodb://heroku_76n6c1p1:G`7vyajtrW'G_(23@ds245927.mlab.com:45927/heroku_76n6c1p1" 
+
+mongodb.MongoClient.connect(uri, (err, client) => {
+    if(err) throw err;
+
+    let db = client.db('heroku_76n6c1p1');
+
+    let sendData = db.collection('data');
+
+    sendData.insert(data, (err, result) => {
+        if(err) throw err;
+        
+    })
+})
