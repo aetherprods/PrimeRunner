509550b92dfd071fa29fc18e9e134cc06f011948
Jorge Barreto <jbarreto@localhost.localdomain>
2019-07-02 11:55:29 -0400

    mongo test

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
 const express = require('express')
       path = require('path')
       mongodb = require('mongodb')
-      //mongoose = require('mongoose')
       port = process.env.PORT || 3000
-      app = express();
+      app = express()
+      bodyParser = require('body-parser');
+
+let uri = "mongodb://heroku_76n6c1p1:u0t3c6j9bqa5c6e6ikbsprtooj@ds245927.mlab.com:45927/heroku_76n6c1p1" 
+let data = [
+    {"a": "1"},
+    {"b": "2"},
+    {"c": "3"},
+]
 
 
-
-//mongoose.connect(process.env.MONGODB_URI || "mongodb://heroku_76n6c1p1:u0t3c6j9bqa5c6e6ikbsprtooj@ds245927.mlab.com:45927/heroku_76n6c1p1");
 
 app.get('/', (req, res) => {res.sendFile(__dirname + "/main.html")});
-//app.use(express.static(__dirname + "/public"));
+
+app.get('/poop', (req, res) => {
+    res.send("Hello Poop!");
+    mongodb.MongoClient.connect(process.env.MONGODB_URI || uri, (err, client) => {
+        if(err) throw err;
+    
+        let db = client.db('heroku_76n6c1p1');
+    
+        let sendData = db.collection('data');
+    
+        sendData.insert(data, (err, result) => {
+            if(err) throw err;
+            
+        })
+    })
+});
 app.use(express.static(path.join(__dirname, "/")));
 app.listen(port, () => console.log("i am zord"));
 
-
-let data=[
-    {"a": 1},
-    {"b": 2},
-    {"c": 3},
-]
-let uri = "mongodb://heroku_76n6c1p1:u0t3c6j9bqa5c6e6ikbsprtooj@ds245927.mlab.com:45927/heroku_76n6c1p1" 
-
-mongodb.MongoClient.connect(uri, (err, client) => {
+/*
+mongodb.MongoClient.connect(process.env.MONGODB_URI || uri, (err, client) => {
     if(err) throw err;
 
     let db = client.db('heroku_76n6c1p1');
@@ -34,3 +47,4 @@ mongodb.MongoClient.connect(uri, (err, client) => {
         
     })
 })
+*/
