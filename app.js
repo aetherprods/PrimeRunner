1ebe316655f3622e7a3ebc021469bc03bb7d8b71
Jorge Barreto <jbarreto@localhost.localdomain>
2019-07-02 05:37:29 -0400

    fucking express

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ const express = require('express'),
 app.get('/poop', (req, res) => res.send("Hello World!"));
 app.get('/', (req, res) => {res.sendFile(__dirname + "/main.html")});
 //app.use(express.static(__dirname + "/public"));
-app.use(express.static(__dirname+"/"))
+app.use(express.static(__dirname+"/public"))
 app.listen(port, () => console.log("i am zord"));
