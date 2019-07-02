af8363cccccd6f432f3e70b2b8cb369a9aa90d8a
Jorge Barreto <jbarreto@localhost.localdomain>
2019-07-02 04:45:32 -0400

    express test

--- main.js
+++ main.js
@@ -6,6 +6,16 @@ todo:
 */
 const $ = require('jquery');
 
+const express = require('express'),
+      port = process.env.PORT || 3000;
+      app = express(),
+
+app.get('/poop', (req, res) => res.send("Hello World!"));
+app.get('/', (req, res) => {
+return res.send("Hello World!");
+});
+app.listen(port, () => console.log("i am zord"));
+
 
 $( document ).ready(function() {
     game = new primeGame();
